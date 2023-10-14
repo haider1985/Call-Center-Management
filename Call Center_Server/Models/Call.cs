caf78f8714e4664d8bc291203d9265df173385b4
Haider F. A. Mohammed Ali <haider.mohammedali@outlook.com>
2023-10-14 19:05:31 +0300

    Delete Default Files and Create Call Class

---
+++ Call Center_Server/Models/Call.cs
@@ -0,0 +1,19 @@
+﻿namespace Call_Center_Server.Models
+{
+    public class Call
+    {
+        public int Id { get; set; }
+        public string CallerName { get; set; } = string.Empty;
+        public DateTime CallDateTime { get; set; } = DateTime.Now;
+        public string CallerCompanyName { get; set; } = string.Empty;
+        public string CallerPhone { get; set;} = string.Empty;
+        public string CallerEmail { get; set; } = string.Empty;
+        public string CallerAddress { get; set; } = string.Empty;
+        public string CallReason { get; set; } = string.Empty;
+        public string CallsUrgency { get; set; } = string.Empty;
+        public string CallStatus { get; set; } = string.Empty;
+        public string ReceivingCallName { get; set; } = string.Empty;
+        public string CallStage { get; set; } = string.Empty;
+
+    }
+}
