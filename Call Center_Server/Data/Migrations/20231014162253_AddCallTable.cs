5f9378861e185a4e1346f1bda3d3a20bf0f31c3a
Haider F. A. Mohammed Ali <haider.mohammedali@outlook.com>
2023-10-14 19:28:21 +0300

    Create Call Table and insert it to database

---
+++ Call Center_Server/Data/Migrations/20231014162253_AddCallTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Call_Center_Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCallTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Calls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CallerName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CallDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CallerCompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CallerPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CallerEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CallerAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CallReason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CallsUrgency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CallStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceivingCallName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CallStage = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calls", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calls");
+        }
+    }
+}
--- Call Center_Server/Models/Call.cs
+++ Call Center_Server/Models/Call.cs
@@ -1,19 +1,59 @@
-﻿namespace Call_Center_Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Call_Center_Server.Models
 {
     public class Call
     {
+        [Required]
         public int Id { get; set; }
-        public string CallerName { get; set; } = string.Empty;
-        public DateTime CallDateTime { get; set; } = DateTime.Now;
-        public string CallerCompanyName { get; set; } = string.Empty;
-        public string CallerPhone { get; set;} = string.Empty;
-        public string CallerEmail { get; set; } = string.Empty;
-        public string CallerAddress { get; set; } = string.Empty;
-        public string CallReason { get; set; } = string.Empty;
-        public string CallsUrgency { get; set; } = string.Empty;
-        public string CallStatus { get; set; } = string.Empty;
-        public string ReceivingCallName { get; set; } = string.Empty;
-        public string CallStage { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(
+            maximumLength: 50,
+            MinimumLength = 3,
+            ErrorMessage = "The caller name must be more than 3 letters and below 50 letters"
+            )]
+        [Display(Name ="Caller Name")]
+        public string CallerName { get; set; } = string.Empty;
+
+        [Display(Name = "Call Date & time")]
+        public DateTime CallDateTime { get; set; } = DateTime.Now;
+
+        [Display(Name = "Company Name")]
+        public string CallerCompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Caller Phone Number")]
+        public string CallerPhone { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Caller Email")]
+        public string CallerEmail { get; set; } = string.Empty;
+
+        [Display(Name = "Company Address")]
+        public string CallerAddress { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Reason of Call")]
+        public string CallReason { get; set; } = string.Empty;
+
+        [Display(Name = "Urgency Level")]
+        public string CallsUrgency { get; set; } = string.Empty;
+
+        [Display(Name = "Status")]
+        public string CallStatus { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(
+           maximumLength: 50,
+           MinimumLength = 3,
+           ErrorMessage = "The receiving call name must be more than 3 letters and below 50 letters"
+           )]
+        [Display(Name = "Receiving Call Name")]
+        public string ReceivingCallName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Stage")]
+        public string CallStage { get; set; } = string.Empty;
     }
 }
