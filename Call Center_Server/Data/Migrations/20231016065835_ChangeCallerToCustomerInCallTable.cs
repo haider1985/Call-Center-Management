0a6c1ac2bcb504e4660b888ec7da9d8e3e8861f0
Haider F. A. Mohammed Ali <haider.mohammedali@outlook.com>
2023-10-16 10:00:00 +0300

    Change Caller to Customer inside database and Call Model

---
+++ Call Center_Server/Data/Migrations/20231016065835_ChangeCallerToCustomerInCallTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Call_Center_Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCallerToCustomerInCallTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReceivingCallName",
+                table: "Calls",
+                newName: "CustomerName");
+
+            migrationBuilder.RenameColumn(
+                name: "CallerPhone",
+                table: "Calls",
+                newName: "CustomerPhone");
+
+            migrationBuilder.RenameColumn(
+                name: "CallerName",
+                table: "Calls",
+                newName: "AgentEmployeeName");
+
+            migrationBuilder.RenameColumn(
+                name: "CallerEmail",
+                table: "Calls",
+                newName: "CustomerEmail");
+
+            migrationBuilder.RenameColumn(
+                name: "CallerCompanyName",
+                table: "Calls",
+                newName: "CustomerCompanyName");
+
+            migrationBuilder.RenameColumn(
+                name: "CallerAddress",
+                table: "Calls",
+                newName: "CustomerCompanyAddress");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CustomerPhone",
+                table: "Calls",
+                newName: "CallerPhone");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerName",
+                table: "Calls",
+                newName: "ReceivingCallName");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerEmail",
+                table: "Calls",
+                newName: "CallerEmail");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerCompanyName",
+                table: "Calls",
+                newName: "CallerCompanyName");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerCompanyAddress",
+                table: "Calls",
+                newName: "CallerAddress");
+
+            migrationBuilder.RenameColumn(
+                name: "AgentEmployeeName",
+                table: "Calls",
+                newName: "CallerName");
+        }
+    }
+}
--- Call Center_Server/Models/CallModel.cs
+++ Call Center_Server/Models/CallModel.cs
@@ -13,27 +13,27 @@ namespace Call_Center_Server.Models
             MinimumLength = 3,
             ErrorMessage = "Invalid Customer Name."
             )]
-        [Display(Name ="Caller Name")]
-        public string CallerName { get; set; } = string.Empty;
+        [Display(Name = "Customer Name")]
+        public string CustomerName { get; set; } = string.Empty;
 
         [Display(Name = "Call Date & time")]
         public DateTime CallDateTime { get; set; } = DateTime.Now;
 
         [Display(Name = "Company Name")]
-        public string CallerCompanyName { get; set; } = string.Empty;
+        public string CustomerCompanyName { get; set; } = string.Empty;
 
         [Required]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^([0-9]{11})$", ErrorMessage = "Invalid Phone Number.")]
-        [Display(Name = "Caller Phone Number")]
-        public string CallerPhone { get; set; } = string.Empty;
+        [Display(Name = "Customer Phone Number")]
+        public string CustomerPhone { get; set; } = string.Empty;
         [Required]
         [DataType(DataType.EmailAddress)]
-        [Display(Name = "Caller Email")]
-        public string CallerEmail { get; set; } = string.Empty;
+        [Display(Name = "Customer Email")]
+        public string CustomerEmail { get; set; } = string.Empty;
 
         [Display(Name = "Company Address")]
-        public string CallerAddress { get; set; } = string.Empty;
+        public string CustomerCompanyAddress { get; set; } = string.Empty;
 
         [Required]
         [Display(Name = "Reason of Call")]
@@ -51,8 +51,8 @@ namespace Call_Center_Server.Models
            MinimumLength = 3,
            ErrorMessage = "Invalid Employee Name"
            )]
-        [Display(Name = "Receiving Call Name")]
-        public string ReceivingCallName { get; set; } = string.Empty;
+        [Display(Name = "Agent Employee Name")]
+        public string AgentEmployeeName { get; set; } = string.Empty;
 
         [Required]
         [Display(Name = "Stage")]
