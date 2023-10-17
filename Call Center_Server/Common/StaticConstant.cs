9dbc2d01512efd3d3c2a52eda48d60e706b0cb01
Haider F. A. Mohammed Ali <haider.mohammedali@outlook.com>
2023-10-17 20:06:54 +0300

    Create Calls Dashboard & Static Constant & Tweak Navbar

---
+++ Call Center_Server/Common/StaticConstant.cs
@@ -0,0 +1,14 @@
+﻿namespace Call_Center_Server.Common
+{
+    public static class StaticConstant
+    {
+        public const string CallStage_New = "New";
+        public const string CallStage_Processing = "Processing";
+        public const string CallStage_Offering = "Offering";
+        public const string CallStage_Pending = "Pending";
+        public const string CallStage_Approved = "Approved";
+        public const string CallStage_Cancelled = "Cancelled";
+        public const string CallStage_Completed = "Completed";
+
+    }
+}
